c574bb640371c87263db8e1109b5ad3007a69669
gowtham <gngowtham25@gmail.com>
2018-08-06 18:11:10 +0530

    Cors filter class is added

--- src/main/java/com/perpule/JerseyApplication.java
+++ src/main/java/com/perpule/JerseyApplication.java
@@ -3,12 +3,8 @@ package com.perpule;
 import org.glassfish.jersey.jackson.JacksonFeature;
 import org.glassfish.jersey.server.ResourceConfig;
 
-import com.perpule.filter.CORSFilter;
-
-
 public class JerseyApplication extends ResourceConfig {
   public JerseyApplication() {
     register(JacksonFeature.class);
-    register(CORSFilter.class);
   }
 }
